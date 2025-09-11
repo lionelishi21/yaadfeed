1f9f0d1548266c029e3d6eee904b19e4c50c5345
Lionel Francis <lionelishmael@gmail.com>
2025-09-11 06:03:52 -0500

    updates

---
+++ src/app/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, we couldn’t find what you’re looking for.</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/pages/500.tsx
@@ -0,0 +1,9 @@
+export default function Custom500() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>500 - Server Error</h1>
+        <p>Something went wrong. Please try again later.</p>
+      </div>
+    );
+  }
+  
