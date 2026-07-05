1712c8e2aa289e1bc4a467835e1cf2e543fe532c
Lionel Francis <lionelfrancis@Lionels-MBP-M2.lan>
2026-07-05 02:44:59 -0500

    chore: add google adsense meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-6524318430609026" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6524318430609026"
